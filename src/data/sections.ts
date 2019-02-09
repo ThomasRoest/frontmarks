44ca636996671510393afb8e52e075690ba059bc
Dmitry Belyaev <blv.dmitry@gmail.com>
2019-02-09 14:44:41 +0100

    Data

--- src/data/sections.ts
+++ src/data/sections.ts
@@ -6,15 +6,14 @@ export const ids = TSection.ids;
 
 export const entities: Entities = {
   [ids.designSystems]: { text: 'Design systems' },
+  [ids.learningCourses]: { text: 'Learn frontend' },
   [ids.frontendNews]: { text: 'Frontend news' },
   [ids.designFreebies]: { text: 'Design freebies' },
-  [ids.learningCourses]: { text: 'Learning courses' },
   [ids.podcasts]: { text: 'Podcasts' },
   [ids.books]: { text: 'Books' },
   [ids.people]: { text: 'People' },
   [ids.conferenceTalks]: { text: 'Conference talks' },
   [ids.developmnetTools]: { text: 'Development tools' },
-  [ids.libraries]: { text: 'Libraries' },
 };
 
 export const response = Object.keys(entities).map(id => ({ id, ...entities[id] }));
