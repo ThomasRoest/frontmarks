bfe35fd998c23f7ebd6cbd56ba81386bca6b1a74
Dmitry Belyaev <blv.dmitry@gmail.com>
2019-02-10 15:23:57 +0100

    Deloy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, './src/public/');
+const BUILD_DIR = path.resolve(__dirname, './public/');
 const APP_DIR = path.resolve(__dirname, './src/');
 const JS_PUBLIC_PATH = '/js/';
 
